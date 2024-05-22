ec9fe574f0e52387fdc580fefb9585a0a761798c
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-05-22 16:06:54 -0400

    stop motor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Commands.MotorStop;
 import frc.robot.Commands.MoveBackward;
 import frc.robot.Commands.MoveForward;
 import frc.robot.Subsystems.Motor;
@@ -23,6 +24,7 @@ public class RobotContainer {
   private void configureBindings() {
     operator.rightTrigger().onTrue(new MoveForward(motor));
     operator.leftTrigger().onTrue(new MoveBackward(motor));
+    operator.rightBumper().onTrue(new MotorStop(motor));
   }
 
   public Command getAutonomousCommand(){
